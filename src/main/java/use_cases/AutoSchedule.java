107e3afb8acc9d600faeb66750cd65cfdeee25a3
Seo Won Yi <sean.yi@mail.utoronto.ca>
2021-10-12 18:33:59 -0400

    more cleaning for organizing

--- src/main/java/use_cases/AutoSchedule.java
+++
@@ -1,8 +0,0 @@
-package use_cases;
-
-import java.time.LocalDateTime;
-
-public interface AutoSchedule {
-    boolean scheduled();
-    void schedule(LocalDateTime startTime, LocalDateTime endTime);
-}
