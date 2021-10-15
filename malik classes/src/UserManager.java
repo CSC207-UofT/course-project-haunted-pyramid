01248a408b5c3d488e135b705501e65ecdd07ecd
Shahzada Shameel <65613195+ShahzadaShameelFarooq@users.noreply.github.com>
2021-10-15 15:55:22 -0400

    Delete malik classes directory

--- malik classes/src/UserManager.java
+++
@@ -1,7 +0,0 @@
-public class UserManager {
-
-    public boolean manageAddCourse(Course course){
-        // check if there are no conflicts with other courses, alert if numberOfCourses greater then 6.
-        return true;
-    }
-}
