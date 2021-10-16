f2a1774e4b9832e9f2d491b6b848c5a9218b7de8
cullenta <taite.cullen@mail.utoronto.ca>
2021-10-16 16:38:49 -0400

    EventManager and Event basic unit tests

---
+++ src/test/java/entities/EventTest.java
@@ -0,0 +1,45 @@
+package entities;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.*;
+
+public class EventTest {
+    Event event1 = new Event(1, "1", LocalDateTime.of(2021, 10, 15, 0, 0,
+            0), LocalDateTime.of(2021, 10, 15, 3, 0, 0));
+    Event event2 = new Event(1, "1", LocalDateTime.of(2021, 10, 15, 0, 0,
+            0), LocalDateTime.of(2021, 10, 15, 3, 0, 0));
+    Event event3 = new Event(1, "3", LocalDateTime.of(2021, 10, 15, 1, 0,
+            0), LocalDateTime.of(2021, 10, 15, 4, 0, 0));
+    Event event4 = new Event(1, "1", LocalDateTime.of(2021, 10, 15, 7, 0,
+            0), LocalDateTime.of(2021, 10, 15, 9, 0, 0));
+    @Test
+    public void testEquals() {
+        assertEquals(this.event1, this.event2);
+    }
+
+    @Test
+    public void getStartString() {
+        assertEquals("2021-10-15 00:00", this.event1.getStartString());
+    }
+
+    @Test
+    public void getEndString() {
+        assertEquals("2021-10-15 03:00", this.event1.getEndString());
+    }
+
+    @Test
+    public void getLength() {
+        assertEquals(2.0, this.event4.getLength(), 0);
+    }
+
+    @Test
+    public void conflicts() {
+        assertTrue(this.event1.conflicts(this.event3));
+        assertFalse(this.event1.conflicts(this.event4));
+    }
+}
--- src/test/java/usecases/EventManagerTest.java
+++ src/test/java/usecases/EventManagerTest.java
@@ -47,7 +47,7 @@ public class EventManagerTest {
 
     @Test (timeout = 100)
     public void testGetEnd() {
-        assertEquals("2021-10-1 02:00", this.eventManager.getStart(this.eventManager.getEvent("1")));
+        assertEquals("2021-10-01 02:00", this.eventManager.getStart(this.eventManager.getEvent("1")));
     }
 
     @Test(timeout = 50)
@@ -66,42 +66,5 @@ public class EventManagerTest {
                 this.eventManager.getEvent("3")));
         assertEquals(ordered, this.eventManager.timeOrder(unordered));
     }
-
-    @Test
-    public void testGetEvent() {
-    }
-
-    @Test
-    public void testRemoveEvent() {
-
-    }
-
-    @Test
-    public void testAddEvent() {
-    }
-
-    @Test
-    public void testTestGetName() {
-    }
-
-
-
-    @Test
-    public void testEarliest() {
-    }
-
-
-    //TODO finish implementing freeSlots
-//    public void testFreeSlots() {
-//        Event slot1 = new Event(1, "slot1", 2021, 10, 1, 3, 4, 0,
-//                0);
-//        Event slot2 = new Event(1, "slot2", 2021, 10, 1, 1, 2, 0,
-//                0);
-//        ArrayList<Event> expected = new ArrayList<Event>(Arrays.asList(slot2, slot1));
-//        ArrayList<Event> slots = this.eventManager.freeSlots(LocalDateTime.of(2021, 10, 1, 1, 0, 0,
-//                0), LocalDateTime.of(2021, 10, 1, 6, 30, 0, 0));
-//        System.out.println(slots.get(2).getName());
-//        assertEquals(expected, slots);
-//    }
 
 }
