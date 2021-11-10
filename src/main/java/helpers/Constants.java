5c117b726e093dcb4626cd9fc9f4cb88ae340492
Seo Won Yi <sean.yi@mail.utoronto.ca>
2021-11-10 12:39:42 -0500

    Added the basic structure of the menu

--- src/main/java/helpers/Constants.java
+++ src/main/java/helpers/Constants.java
@@ -1,9 +1,14 @@
 package helpers;
 
 public class Constants {
+    // CALENDAR_SIZE CAN ONLY INCREASE BY MULTIPLES OF 14
+    // CAL_ROW_SPACER AND WEEKLY_CAL_NAME_LIMIT CAN BE INCREASED IFF CALENDAR_SIZE INCREASES
+    // WHEN CALENDAR_SIZE INCREASES, CAL_ROW_SPACER INCREASES BY THE AMOUNT DIVIDED BY 7
+    // WEEKLY_CAL_NAME_LIMIT CAN ONLY BE AROUND ROUGHLY CAL_ROW_SPACER - 10
     public static final int CALENDAR_SIZE = 224;
     public static final int DAILY_CAL_SIZE = 100;
     public static final int CAL_ROW_SPACER = 24;
     public static final int WEEKLY_CAL_NAME_LIMIT = 14;
+    public static final int MENU_DIVIDER = 35;
 
 }
--- src/main/java/presenters/DisplayMenu.java
+++ src/main/java/presenters/DisplayMenu.java
@@ -1,7 +1,32 @@
 package presenters;
 
-public class DisplayMenu {
-    public DisplayMenu(){
+import helpers.Constants;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisplayMenu {
+    private MenuContent menuContent = new MenuContent();
+
+
+    public String displayMenu(String type){
+        StringBuilder result = new StringBuilder();
+        String divider = "-".repeat(Constants.MENU_DIVIDER);
+        result.append(" ").append(divider).append(" ");
+        if (type.equalsIgnoreCase("Basic"))
+        {
+            this.menuContent.addBasicMenuContent(result);}
+        else if (type.equalsIgnoreCase("Event Type")){
+            this.menuContent.addEventTypeMenuContent(result);
+        }
+        result.append("\n").append(" ").append(divider);
+        return result.toString();
+    }
+
+
+    public static void main(String[] args) {
+        DisplayMenu dm = new DisplayMenu();
+        System.out.println(dm.displayMenu("basic"));
+        System.out.println(dm.displayMenu("event type"));
     }
 }
