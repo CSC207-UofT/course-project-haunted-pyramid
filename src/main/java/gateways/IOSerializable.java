2a4516cabaceba49f627e9f1daf2f43decabc6e7
Sebin <sebinim4e@gmail.com>
2021-11-11 13:31:33 -0500

    Bugfixes.

--- src/main/java/gateways/IOSerializable.java
+++ src/main/java/gateways/IOSerializable.java
@@ -32,8 +32,8 @@ public class IOSerializable {
     private static final String ACCESS_TOKEN = "EfBUX9G7zxkAAAAAAAAAAaXr-kGtiOL1cwBhwIe7BcI0hvt-uH5LBsEh4FXJ31Ry";
 
     // A public Dropbox link where the serialized files are stored
-    private static final String eventsURL = "https://www.dropbox.com/s/lzdvzsu1zwyeqkf/events.ser?dl=1";
-    private static final String usersURL = "https://www.dropbox.com/s/pcbd8c2oyueq877/users.ser?dl=1";
+    private static final String eventsURL = "https://www.dropbox.com/s/1zc7viholnesb52/events.ser?dl=1";
+    private static final String usersURL = "https://www.dropbox.com/s/x20ggjrddr6cy1x/users.ser?dl=1";
 
     public IOSerializable(Boolean intro) {
         readFromDropbox(intro);
