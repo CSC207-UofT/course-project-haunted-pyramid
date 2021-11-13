1218fd5d23a74bbcdd0485f63e663054f0b6fc5a
Type your name here <malik.lahlou@mail.utoronto.ca>
2021-11-12 21:44:50 -0500

    removed main method in IntervalDateInput

--- src/main/java/entities/recursions/IntervalDateInput.java
+++ src/main/java/entities/recursions/IntervalDateInput.java
@@ -98,23 +98,4 @@ public class IntervalDateInput implements DateGetter {
         events.add(firstEvent2);
         return result;
     }
-
-    public static void main(String[] args) {
-        LocalDateTime l =  LocalDateTime.of(2021, 11, 15, 11,0);
-        LocalDateTime l2 =  LocalDateTime.of(2021, 12, 17, 11,0);
-        LocalDateTime l3 =  LocalDateTime.of(2021, 12, 31, 11,0);
-        LocalDateTime l4 =  LocalDateTime.of(2021, 11, 20, 11,0);
-        Event e1 = new Event(1, "e1", l);
-        Event e2 = new Event(2, "e2", 2021, 11, 18, 10, 11, 0, 0);
-        Event e3 = new Event(3, "e3", 2021, 11, 20, 10, 11, 0, 0);
-        ArrayList<Event> z = new ArrayList<>();
-        z.add(e1);
-        z.add(e2);
-        z.add(e3);
-        IntervalDateInput x = new IntervalDateInput(l2, l3);
-        ArrayList<Event> y = x.listOfDatesInCycles(z);
-        for(Event i:y){
-            System.out.println(i);
-        }
-    }
 }
