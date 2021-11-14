112a1ce8f5eaa90dfdfb59078912b1c6ace38bf9
Seo Won Yi <sean.yi@mail.utoronto.ca>
2021-11-14 12:06:43 -0500

    Added description of procrastinate

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -53,8 +53,9 @@ public class UserController {
         while (!done){
             DisplayMenu dm = new DisplayMenu();
             ProfileMenuContent profileMenuContent = new ProfileMenuContent(this.currentUser, this.getUserManager());
-            System.out.println("Note: Work Sessions will only set up during Free Time");
-            System.out.println("      If Procrastinate is on, Work Sessions will be scheduled more towards the deadline");
+            System.out.println("Note:");
+            System.out.println("Work Sessions will only set up during Free Time");
+            System.out.println("If Procrastinate is on, Work Sessions will be scheduled more towards the deadline");
             String firstAction = ioController.getAnswer(dm.displayMenu(profileMenuContent));
             firstAction = helper.invalidCheck(dm, firstAction, profileMenuContent.numberOfOptions(), profileMenuContent);
             if (firstAction.equalsIgnoreCase("Return")){
