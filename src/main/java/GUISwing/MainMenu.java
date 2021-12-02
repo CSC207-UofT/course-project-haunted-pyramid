1013d18602f4497eae70fcdf608bf010d16fce9c
Seo Won Yi <sean.yi@mail.utoronto.ca>
2021-12-02 16:46:15 -0500

    removing error

--- src/main/java/GUISwing/MainMenu.java
+++ src/main/java/GUISwing/MainMenu.java
@@ -4,7 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainMenu {
+    private JFrame frame;
+
     public MainMenu(String name, JFrame frame) {
+        this.frame = frame;
         JLabel welcomeMessage = new JLabel();
         welcomeMessage.setText("Welcome " + name + "!");
         welcomeMessage.setHorizontalTextPosition(JLabel.CENTER);
@@ -14,11 +17,17 @@ public class MainMenu {
         welcomeMessage.setVerticalAlignment(JLabel.CENTER);
         welcomeMessage.setHorizontalAlignment(JLabel.CENTER);
         welcomeMessage.setBounds(1444/3, 0, 1444/3, 200);
-        frame.add(welcomeMessage);
+        this.frame.add(welcomeMessage);
+        this.frame.setVisible(false);
+    }
+
+    public void display() {
         frame.setVisible(true);
     }
 
+
     public static void main(String[] args) {
-        new MainMenu("Sean", new MainFrame());
+        MainMenu mainMenu = new MainMenu("Sean", new MainFrame());
+        mainMenu.display();
     }
 }
