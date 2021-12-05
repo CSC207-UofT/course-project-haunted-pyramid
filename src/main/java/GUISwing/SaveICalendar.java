39cd3c10f41dc69c11f9e01e68dd78627cb37409
Seo Won Yi <sean.yi@mail.utoronto.ca>
2021-12-04 21:49:57 -0500

    save ICalendar implemented!

--- src/main/java/GUISwing/SaveICalendar.java
+++ src/main/java/GUISwing/SaveICalendar.java
@@ -6,19 +6,25 @@ import gateways.ICalendar;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
+import java.io.IOException;
 
 public class SaveICalendar {
     public void save(EventController eventController) {
         JFileChooser jFileChooser = new JFileChooser();
         FileNameExtensionFilter filter = new FileNameExtensionFilter("iCal Format", "ics");
         jFileChooser.setDialogTitle("Save iCalendar");
-        jFileChooser.setDialogType(2);
         jFileChooser.setFileFilter(filter);
         int response = jFileChooser.showSaveDialog(null);
         if (response == JFileChooser.APPROVE_OPTION) {
-            File file = new File(jFileChooser.getSelectedFile().getAbsolutePath());
+            File file = jFileChooser.getSelectedFile().getAbsoluteFile();
             ICalendar iCalendar = new ICalendar(eventController.getEventManager());
-            iCalendar.create(file.toString());
+            try {
+                iCalendar.create(file.toString());
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null,
+                        "Please type a valid file name", "File Name Error", JOptionPane.WARNING_MESSAGE);
+                save(eventController);
+            }
         }
     }
 }
